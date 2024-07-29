2f3c594480ae1c9d3cb55e7ac76ae01c348255bd
Илья <meowingless@mail.ru>
2024-07-30 00:42:18 +0300

    routes fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { CountryDetails } from './components/CountryDetails';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test-task-countries/">
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/country/:name' element={<CountryDetails />} />
